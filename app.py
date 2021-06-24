90d99448ba11b1cb85c34e398253e585122b80ca
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-06-23 22:35:15 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import datetime
+import pandas as pd
+import numpy as np
+import random
+import os
+from requests import get
+
+endpoint = "https://api-model-engine-chatbot.juanengml.repl.co/bot"
+
+
+st.set_page_config(
+  page_title="Análise Comportamento Consumidor",
+  page_icon="💰",
+  layout="centered",
+  initial_sidebar_state="expanded",)
+
+def main(): 
+    st.title(" 🤖 Meu ChatBot")
+    
+    me = st.text_area(" 😃 Me:", value="Escreva", key=None)
+    if st.button(" ✉️ Enviar") == True:
+           data = {'pergunta': me}
+           r = get(endpoint,data).json()
+          # st.write(data)
+           #st.write(r)
+           st.text_area(" 🤖 Bot:",r['bot']['GeorgeBot'], key=None)
+
+        
+if __name__ == "__main__":
+    main()
